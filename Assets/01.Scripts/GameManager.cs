3dd5c0a8117325841f84bc30adceff9f4f567b92
subaruboshi <gimunam02@gmail.com>
2025-03-10 12:09:52 +0900

    20250310
    라스트 파이널 - 닷지 완료

--- Assets/01.Scripts/GameManager.cs
+++ Assets/01.Scripts/GameManager.cs
@@ -22,12 +22,47 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
-        
+        //게임오버가 아닌 동안
+        if(!isGameover)
+        {
+            //생존 시간 갱신
+            surviveTime += Time.deltaTime;
+            //갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
+            timeText.text = "Time" + (int)surviveTime;
+        }
+        else
+        {
+            //게임오버 상태에서 R 키를 누른 경우
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                //SampleScene 씬을 로드
+                SceneManager.LoadScene("SampleScene");
+            }
+
+        }
     }
     //현재 게임을 게임오버 상태로 변경하는 메서드
     public void EndGame()
     {
+        //현재 상태를 게임오버 상태로 전환
+        isGameover = true;
+        //게임오버 텍스트 게임 오브젝트를 활성화
+        gameoverText.SetActive(true);
 
+        //BestTime 키로 저장된 이전까지의 최고 기록 가져오기
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        //이전까지의 최고 기록보다 현재 생존 시간이 더 크다면
+        if(surviveTime>bestTime)
+        {
+            //최고 기록 값을 현재 생존 시간값으로 변경 
+            bestTime = surviveTime;
+            //변경된 최고 기록을 BestTime 키로 저장
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        //최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
+        recordText.text = "Best Time:" + (int)bestTime;
     }
 
 }
